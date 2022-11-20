16ea5ff1e2e26ad28afbd5a68a89453a52800a19
tboulet <timothe.boulet0@gmail.com>
2022-11-20 01:25:46 +0100

    setup 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     
     packages=find_namespace_packages(),
 
-    version="1.0",
+    version="1.1",
     license="MIT",
     description="Optimization and constraint satisfaction for integer linear programs in a user-friendly way",
     long_description=open('README.md').read(),      # always in md, with a README.md (convention!)
